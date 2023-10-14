9205d5ad2d2f9db981f53e3915dcf457f72c540a
leo115129 <1792664321@qq.com>
2023-10-14 20:11:11 +0800

    gateway

--- gateway/src/main/java/com/example/train/gateway/config/GateWayApplication.java
+++ gateway/src/main/java/com/example/train/gateway/config/GateWayApplication.java
@@ -1,4 +1,4 @@
-package com.example.train.member.config;
+package com.example.train.gateway.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,14 +10,14 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.example")
-public class MemberApplication {
+public class GateWayApplication {
 
-    private static final Logger LOG= LoggerFactory.getLogger(MemberApplication.class);
+    private static final Logger LOG= LoggerFactory.getLogger(GateWayApplication.class);
     public static void main(String[] args) {
-        SpringApplication app=new SpringApplication(MemberApplication.class);
+        SpringApplication app=new SpringApplication(GateWayApplication.class);
         Environment env=app.run(args).getEnvironment();
         LOG.info("启动成功!!!");
-        LOG.info("测试地址:\thttp://127.0.0.1:{}/{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
+        LOG.info("网关地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
