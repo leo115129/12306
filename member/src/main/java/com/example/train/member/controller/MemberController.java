85987b6f3de069a08d301663050a487873dcb244
leo115129 <1792664321@qq.com>
2023-10-15 09:32:05 +0800

    集成持久层Mybatis,访问数据库成功

---
+++ member/src/main/java/com/example/train/member/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.example.train.member.controller;
+
+import com.example.train.member.service.MemberService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @Resource
+    private MemberService memberService;
+
+    @GetMapping("/count")
+    public int count(){return memberService.count();}
+}
---
+++ member/src/main/java/com/example/train/member/mapper/MemberMapper.java
@@ -0,0 +1,6 @@
+package com.example.train.member.mapper;
+
+public interface MemberMapper {
+
+    int count();
+}
