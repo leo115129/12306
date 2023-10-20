926014b12d18cadc3b025a2bf489b0827fe2b079
leo115129 <1792664321@qq.com>
2023-10-20 16:42:58 +0800

    增加Business业务模块

--- business/src/main/java/com/example/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/example/train/business/config/BusinessApplication.java
@@ -1,4 +1,4 @@
-package com.example.train.member.config;
+package com.example.train.business.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
@@ -11,12 +11,12 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.example")
-@MapperScan("com.example.train.member.mapper")
-public class MemberApplication {
+@MapperScan("com.example.train.*.mapper")
+public class BusinessApplication {
 
-    private static final Logger LOG= LoggerFactory.getLogger(MemberApplication.class);
+    private static final Logger LOG= LoggerFactory.getLogger(BusinessApplication.class);
     public static void main(String[] args) {
-        SpringApplication app=new SpringApplication(MemberApplication.class);
+        SpringApplication app=new SpringApplication(BusinessApplication.class);
         Environment env=app.run(args).getEnvironment();
         LOG.info("启动成功!!!");
         LOG.info("测试地址:\thttp://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
